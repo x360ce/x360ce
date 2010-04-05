2ea92938335d5d87aaea1af292fadab4b3652ede
Evaldas Jocys <ejocys@users.noreply.github.com>
2010-04-05 10:51:31 +0000

    New: "Saitek Cyborg Rumble Pad" preset by Jorge Fuentes

--- x360ce.App/MainForm.cs
+++ x360ce.App/MainForm.cs
@@ -113,17 +113,20 @@ namespace x360ce.App
 			{
 				return;
 			}
-			
+
 			ReloadXinputSettings();
 			Version v = new Version(Application.ProductVersion);
 			this.Text = string.Format(this.Text, Application.ProductVersion);
+			// Version = major.minor.build.revision
 			switch (v.Build)
 			{
-				case 0: this.Text += " Alpha"; break;
-				case 1: this.Text += " Beta 1"; break;
-				case 2: this.Text += " Beta 2"; break;
-				case 3: this.Text += " Beta 3"; break;
-				case 4: this.Text += " RC"; break;
+				case 0: this.Text += " Alpha"; break;  // Alpha Release (AR)
+				case 1: this.Text += " Beta 1"; break; // Master Beta (MB)
+				case 2: this.Text += " Beta 2"; break; // Feature Complete (FC)
+				case 3: this.Text += " Beta 3"; break; // Technical Preview (TP)
+				case 4: this.Text += " RC"; break;     // Release Candidate (RC)
+				// case 5: this.Text += " RTM"; break; // Release to Manufacturing (RTM)
+				// case 6: this.Text += " GM"; break;  // General Availability (GA) / Gold
 			}
 			//InitDirectInputTab();
 			// Timer will execute ReloadXInputLibrary();
@@ -470,10 +473,10 @@ namespace x360ce.App
 				HelpRichTextBox.DeselectAll();
 			}
 		}
-	
 
-	
-		
+
+
+
 
 	}
 }
--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.16")]
+[assembly: AssemblyVersion("2.0.2.17")]
 [assembly: AssemblyDelaySign(false)]
