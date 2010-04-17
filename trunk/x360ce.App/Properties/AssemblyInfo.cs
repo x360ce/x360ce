f0bd84688436981ac0c1de2b20765c7a1e60f230
ejocys <ejocys@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-04-17 11:01:35 +0000

    Changelog updated. Updated to v2.0.2.33

--- trunk/x360ce.App/Properties/AssemblyInfo.cs
+++ trunk/x360ce.App/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.29")]
+[assembly: AssemblyVersion("2.0.2.33")]
 [assembly: AssemblyDelaySign(false)]
