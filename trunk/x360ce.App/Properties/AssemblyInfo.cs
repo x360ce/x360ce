5e64c1773095c8c28c852600c7643d608fd080db
ejocys <ejocys@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-04-23 23:01:37 +0000

    v2.0.2.46 Update: Updated to xinput1_3.dll v3.2.0.97.

--- trunk/x360ce.App/Properties/AssemblyInfo.cs
+++ trunk/x360ce.App/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.44")]
+[assembly: AssemblyVersion("2.0.2.46")]
 [assembly: AssemblyDelaySign(false)]
