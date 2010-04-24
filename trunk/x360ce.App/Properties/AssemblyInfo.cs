93dfdb3aaca8a7b669a78a5dfe111f18808e814c
ejocys <ejocys@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-04-24 20:21:16 +0000

    state query fix

--- trunk/x360ce.App/Properties/AssemblyInfo.cs
+++ trunk/x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.51")]
-[assembly: AssemblyFileVersionAttribute("2.0.2.51")]
+[assembly: AssemblyVersion("2.0.2.52")]
+[assembly: AssemblyFileVersionAttribute("2.0.2.52")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
