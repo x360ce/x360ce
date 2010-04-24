203d17b7f9eacba696e2d1852c5de862c92c16fe
Evaldas Jocys <ejocys@users.noreply.github.com>
2010-04-24 19:07:42 +0000

    version update to 2.0.2.49

--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.46")]
-[assembly: AssemblyFileVersionAttribute("2.0.2.46")]
+[assembly: AssemblyVersion("2.0.2.49")]
+[assembly: AssemblyFileVersionAttribute("2.0.2.49")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
