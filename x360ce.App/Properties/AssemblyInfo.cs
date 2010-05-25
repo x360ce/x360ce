8cedcaf843f46a2439c100b206603eadced2e55d
Evaldas Jocys <ejocys@users.noreply.github.com>
2010-05-25 21:56:15 +0000

    xinput fix fix

--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.60")]
-[assembly: AssemblyFileVersionAttribute("2.0.2.60")]
+[assembly: AssemblyVersion("2.0.2.61")]
+[assembly: AssemblyFileVersionAttribute("2.0.2.61")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
