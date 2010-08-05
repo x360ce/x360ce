d9145637095a4868db47ae2fdf376b5546be3870
ejocys <ejocys@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-08-05 17:35:24 +0000

    App update to xinput1_3.dll 3.2.0.138

--- trunk/x360ce.App/Properties/AssemblyInfo.cs
+++ trunk/x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.66")]
-[assembly: AssemblyFileVersionAttribute("2.0.2.66")]
+[assembly: AssemblyVersion("2.0.2.67")]
+[assembly: AssemblyFileVersionAttribute("2.0.2.67")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
