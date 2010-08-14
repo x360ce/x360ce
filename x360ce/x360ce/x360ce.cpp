50e97cdd8002494224b17ddc4cbf97f03371d487
tapcio <tapcio@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-08-14 21:40:59 +0000

    Comment zeros, ZeroMemory does the job faster.

--- x360ce/x360ce/x360ce.cpp
+++ x360ce/x360ce/x360ce.cpp
@@ -395,19 +395,19 @@ extern "C" DWORD WINAPI XInputGetCapabilities(DWORD dwUserIndex, DWORD dwFlags, 
 	if(!capsready[dwUserIndex]) {
 		ZeroMemory(&XCAPS[dwUserIndex],sizeof(XINPUT_CAPABILITIES));
 
-		XCAPS[dwUserIndex].Type = 0;
+		//XCAPS[dwUserIndex].Type = 0;
 		XCAPS[dwUserIndex].SubType = Gamepad[dwUserIndex].gamepadtype;
-		XCAPS[dwUserIndex].Flags = 0;
+		//XCAPS[dwUserIndex].Flags = 0;
 		XCAPS[dwUserIndex].Vibration.wLeftMotorSpeed = XCAPS[dwUserIndex].Vibration.wRightMotorSpeed = 0xFFFF;
 
 		XCAPS[dwUserIndex].Gamepad.wButtons = 0xFFFF;	
 		XCAPS[dwUserIndex].Gamepad.bLeftTrigger = 0xFF;
 		XCAPS[dwUserIndex].Gamepad.bRightTrigger = 0xFF;
 		//center is more reliable because SHORT is signed
-		XCAPS[dwUserIndex].Gamepad.sThumbLX = 0;
-		XCAPS[dwUserIndex].Gamepad.sThumbLY = 0;
-		XCAPS[dwUserIndex].Gamepad.sThumbRX = 0;
-		XCAPS[dwUserIndex].Gamepad.sThumbRY = 0;
+		//XCAPS[dwUserIndex].Gamepad.sThumbLX = 0;
+		//XCAPS[dwUserIndex].Gamepad.sThumbLY = 0;
+		//XCAPS[dwUserIndex].Gamepad.sThumbRX = 0;
+		//XCAPS[dwUserIndex].Gamepad.sThumbRY = 0;
 		capsready[dwUserIndex] = true;
 	}
 
