346cb72c4cd87bd046e1f2c171b3781053f14263
tapcio <tapcio@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-08-20 18:33:57 +0000

    upps, now fully revert to r259

--- x360ce/x360ce/x360ce.cpp
+++ x360ce/x360ce/x360ce.cpp
@@ -353,8 +353,15 @@ extern "C" DWORD WINAPI XInputSetState(DWORD dwUserIndex, XINPUT_VIBRATION* pVib
 	WORD wLeftMotorSpeed = 0;
 	WORD wRightMotorSpeed = 0;
 
-	PrepareForce(dwUserIndex,Gamepad[dwUserIndex].swapmotor?0:1);
-	PrepareForce(dwUserIndex,Gamepad[dwUserIndex].swapmotor?1:0);
+	if(!Gamepad[dwUserIndex].ff.forceready) {
+		HRESULT hrLeftForce = E_FAIL, hrRightForce = E_FAIL;
+		hrLeftForce = PrepareForce(dwUserIndex,Gamepad[dwUserIndex].swapmotor?0:1);
+		hrRightForce = PrepareForce(dwUserIndex,Gamepad[dwUserIndex].swapmotor?1:0);
+		if FAILED(hrLeftForce) WriteLog(_T("[XINPUT] PrepareForce for pad %d failed with code hrLeftForce = %s"), dwUserIndex, DXErrStr(hrLeftForce));
+		if FAILED(hrRightForce) WriteLog(_T("[XINPUT] PrepareForce for pad %d failed with code hrRightForce = %s"), dwUserIndex, DXErrStr(hrRightForce));
+		if (SUCCEEDED(hrLeftForce) && SUCCEEDED(hrRightForce))
+			Gamepad[dwUserIndex].ff.forceready = TRUE;
+	}
 
 	if(Gamepad[dwUserIndex].swapmotor) {
 		wRightMotorSpeed = (WORD)((FLOAT)pVibration->wLeftMotorSpeed * Gamepad[dwUserIndex].ff.forcepercent);
@@ -365,11 +372,12 @@ extern "C" DWORD WINAPI XInputSetState(DWORD dwUserIndex, XINPUT_VIBRATION* pVib
 		wRightMotorSpeed = (WORD)((FLOAT)pVibration->wRightMotorSpeed * Gamepad[dwUserIndex].ff.forcepercent);
 	}
 
-	hr = SetDeviceForces(dwUserIndex,wLeftMotorSpeed,0);
-	if(FAILED(hr))WriteLog(_T("[XINPUT] SetDeviceForces for pad %d failed with code HR = %s"), dwUserIndex, DXErrStr(hr));
-	hr = SetDeviceForces(dwUserIndex,wRightMotorSpeed,1);
-	if(FAILED(hr))WriteLog(_T("[XINPUT] SetDeviceForces for pad %d failed with code HR = %s"), dwUserIndex, DXErrStr(hr));
-
+	if(Gamepad[dwUserIndex].ff.forceready) {
+		hr = SetDeviceForces(dwUserIndex,wLeftMotorSpeed,0);
+		if(FAILED(hr))WriteLog(_T("[XINPUT] SetDeviceForces for pad %d failed with code HR = %s"), dwUserIndex, DXErrStr(hr));
+		hr = SetDeviceForces(dwUserIndex,wRightMotorSpeed,1);
+		if(FAILED(hr))WriteLog(_T("[XINPUT] SetDeviceForces for pad %d failed with code HR = %s"), dwUserIndex, DXErrStr(hr));
+	}
 	return ERROR_SUCCESS;
 }
 
