a257bfa0814c773985b70241f4111f55f54e3566
tapcio <tapcio@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2010-10-21 13:35:05 +0000

    Split FakeAPI and x360ce.

---
+++ x360ce/FakeAPI/FakeWinTrust.cpp
@@ -0,0 +1,58 @@
+/*  x360ce - XBOX360 Controler Emulator
+*  Copyright (C) 2002-2010 ToCA Edit
+*
+*  x360ce is free software: you can redistribute it and/or modify it under the terms
+*  of the GNU Lesser General Public License as published by the Free Software Found-
+*  ation, either version 3 of the License, or (at your option) any later version.
+*
+*  x360ce is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+*  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*  PURPOSE.  See the GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License along with x360ce.
+*  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "stdafx.h"
+#include "globals.h"
+#include "Utils.h"
+#include <Softpub.h>
+#include "FakeAPI.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+LONG (WINAPI *OriginalWinVerifyTrust)(HWND hwnd, GUID *pgActionID,LPVOID pWVTData) = NULL;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LONG WINAPI FakeWinVerifyTrust(HWND hwnd, GUID *pgActionID,LPVOID pWVTData)
+{
+	if(!FakeAPI_Config()->bEnabled) return FakeWinVerifyTrust(hwnd,pgActionID,pWVTData);
+
+	UNREFERENCED_PARAMETER(hwnd);
+	UNREFERENCED_PARAMETER(pgActionID);
+	UNREFERENCED_PARAMETER(pWVTData);
+	return 0;
+}
+
+void FakeWinTrust()
+{
+	if(!OriginalWinVerifyTrust) {
+		OriginalWinVerifyTrust = WinVerifyTrust;
+
+		WriteLog(L"[FAKEAPI] FakeWinTrust:: Attaching");
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		DetourAttach(&(PVOID&)OriginalWinVerifyTrust, FakeWinVerifyTrust);
+		DetourTransactionCommit();
+	}
+}
+
+void FakeWinTrust_Clean()
+{
+	if(OriginalWinVerifyTrust) {
+		WriteLog(L"[FAKEWT]  FakeWinVerifyTrust:: Detaching");
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		DetourDetach(&(PVOID&)OriginalWinVerifyTrust, FakeWinVerifyTrust);
+		DetourTransactionCommit();
+	}
+}
--- x360ce/x360ce/DirectInput.h
+++ x360ce/x360ce/DirectInput.h
@@ -16,8 +16,6 @@
 #ifndef _DIRECTINPUT_H_
 #define _DIRECTINPUT_H_
 
-#define DIRECTINPUT_VERSION 0x0800
-
 #include <dinput.h>
 
 struct DINPUT_DATA 
