2194d3439675c183ad11538811711ea4b807c1b0
ejocys <ejocys@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2011-07-13 20:49:44 +0000

    version updated to v2.0.2.105 (2011-07-13)

--- trunk/x360ce.App/Properties/AssemblyInfo.cs
+++ trunk/x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.2.102")]
-[assembly: AssemblyFileVersionAttribute("2.0.2.102")]
+[assembly: AssemblyVersion("2.0.2.105")]
+[assembly: AssemblyFileVersionAttribute("2.0.2.105")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
