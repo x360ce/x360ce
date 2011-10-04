4ac255dd055515a1dea607632a1be51c1773b313
Evaldas Jocys <ejocys@users.noreply.github.com>
2011-10-04 22:10:47 +0000

    Support for Hook Mode 'All' added.

--- x360ce.App/Common/HookMode.cs
+++ x360ce.App/Common/HookMode.cs
@@ -8,5 +8,7 @@
 		Normal = 1,
 		/// <summary>Faking FakeWMI + FakeDI, ie will spoof all needed data to game, will make (almost) XBOX 360 Controller for Windows.</summary>
 		Compatibility = 2,
+		/// <summary></summary>
+		All = 3
 	}
 }
