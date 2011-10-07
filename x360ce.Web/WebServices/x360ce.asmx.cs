5cb4ce0c74e536bc653b7bceb85a329874e1b32b
Evaldas Jocys <ejocys@users.noreply.github.com>
2011-10-07 21:09:39 +0000

    Web function fix and non required code removed.

--- x360ce.Web/WebServices/x360ce.asmx.cs
+++ x360ce.Web/WebServices/x360ce.asmx.cs
@@ -128,7 +128,7 @@ namespace x360ce.Web.WebServices
 			// Global Configurations.
 			var products = args.Where(x => x.ProductGuid != Guid.Empty).Select(x => x.ProductGuid).Distinct().ToArray();
 			var files = args.Where(x => !string.IsNullOrEmpty(x.FileName) || !string.IsNullOrEmpty(x.FileProductName)).ToList();
-			if (products.Length == 0 || files.Count == 0)
+			if (products.Length == 0)
 			{
 				sr.Summaries = new Summary[0];
 			}
@@ -182,93 +182,6 @@ namespace x360ce.Web.WebServices
 			return sr;
 		}
 
-
-	}
-
-	public class ParameterRebinder : ExpressionVisitor
-	{
-
-		private readonly Dictionary<ParameterExpression, ParameterExpression> map;
-
-
-
-		public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
-		{
-
-			this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
-
-		}
-
-
-
-		public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
-		{
-
-			return new ParameterRebinder(map).Visit(exp);
-
-		}
-
-
-
-		protected override Expression VisitParameter(ParameterExpression p)
-		{
-
-			ParameterExpression replacement;
-
-			if (map.TryGetValue(p, out replacement))
-			{
-
-				p = replacement;
-
-			}
-
-			return base.VisitParameter(p);
-
-		}
-
-	}
-
-	public static class Utility
-	{
-
-		public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
-		{
-
-			// build parameter map (from parameters of second to parameters of first)
-
-			var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
-
-
-
-			// replace parameters in the second lambda expression with parameters from the first
-
-			var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
-
-
-
-			// apply composition of lambda expression bodies to parameters from the first expression 
-
-			return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
-
-		}
-
-
-
-		public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
-		{
-
-			return first.Compose(second, Expression.And);
-
-		}
-
-
-
-		public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
-		{
-
-			return first.Compose(second, Expression.Or);
-
-		}
 
 	}
 
