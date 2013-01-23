5913455f432355485d6c50e25910d21305c716eb
Evaldas Jocys <ejocys@users.noreply.github.com>
2013-01-23 21:30:21 +0000

    version update to 2.1.2.186

--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.2.184")]
-[assembly: AssemblyFileVersionAttribute("2.1.2.184")]
+[assembly: AssemblyVersion("2.1.2.186")]
+[assembly: AssemblyFileVersionAttribute("2.1.2.186")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
