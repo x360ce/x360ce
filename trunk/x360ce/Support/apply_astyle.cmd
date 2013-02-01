225021a0df0b3dba879345de3a4ee25ba62c4f88
tapcio@gmail.com <tapcio@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2013-02-01 21:48:14 +0000

    Upps, I did it again :p Properly applying Astyle.
    PS: VS can also set indent to 4 spaces (Tools->Options->Text Editor ->C.C++->Tabs-> mark Insert spaces, tab/indent size 4)

--- trunk/x360ce/Support/apply_astyle.cmd
+++ trunk/x360ce/Support/apply_astyle.cmd
@@ -1,4 +1,4 @@
-AStyle.exe --recursive --style=allman --indent=spaces ..\dinput8\*.cpp ..\x360ce\*.h
+AStyle.exe --recursive --style=allman --indent=spaces ..\x360ce\*.cpp ..\x360ce\*.h
 AStyle.exe --recursive --style=allman --indent=spaces ..\dinput8\*.cpp ..\dinput8\*.h
 AStyle.exe --recursive --style=allman --indent=spaces ..\3rdparty\libMinHook\*.cpp ..\3rdparty\libMinHook\*.h
 pause
