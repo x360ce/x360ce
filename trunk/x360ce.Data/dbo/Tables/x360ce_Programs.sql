33d2b12fd3e46b41febacdea9441ded300bd1b89
ejocys@gmail.com <ejocys@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2013-02-12 08:28:49 +0000

    Preparing SQL for Programs/Games database.

---
+++ trunk/x360ce.Data/dbo/Tables/x360ce_Programs.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[x360ce_Programs] (
+    [ProgramId]       UNIQUEIDENTIFIER NOT NULL,
+    [FileName]        NVARCHAR (128)   CONSTRAINT [DF_x360ce_Programs_FileName] DEFAULT ('') NOT NULL,
+    [FileProductName] NVARCHAR (256)   CONSTRAINT [DF_x360ce_Programs_FileProductName] DEFAULT ('') NOT NULL,
+    [HookMask]        INT              CONSTRAINT [DF_x360ce_Programs_HookMask] DEFAULT ((0)) NOT NULL,
+    [XInputFileName]  NVARCHAR (128)   CONSTRAINT [DF_x360ce_Programs_XInputFileName] DEFAULT (N'xinput1_3.dll') NOT NULL,
+    CONSTRAINT [PK_x360ce_Programs] PRIMARY KEY CLUSTERED ([ProgramId] ASC)
+);
+
