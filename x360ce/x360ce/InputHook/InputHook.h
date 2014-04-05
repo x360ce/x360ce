179341c9be3551a01278ad5afdfd8e0a64a35d53
tapcio@gmail.com <tapcio@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2014-04-05 13:35:31 +0000

    Added InputHook HookNoTimeout mask.

--- x360ce/x360ce/InputHook/InputHook.h
+++ x360ce/x360ce/InputHook/InputHook.h
@@ -31,42 +31,42 @@ class iHookDevice
 {
 public:
 	iHookDevice(DWORD userindex, const GUID& productid, const GUID& instanceid)
-        :m_enabled(true)
-        ,m_productid(productid)
-        ,m_instanceid(instanceid)
-		,m_userindex(userindex)
-    {}
-    virtual ~iHookDevice() {};
+		:m_enabled(true)
+		, m_productid(productid)
+		, m_instanceid(instanceid)
+		, m_userindex(userindex)
+	{}
+	virtual ~iHookDevice() {};
 
-    inline void Enable()
-    {
-        m_enabled = true;
-    }
+	inline void Enable()
+	{
+		m_enabled = true;
+	}
 
-    inline void Disable()
-    {
-        m_enabled = false;
-    }
+	inline void Disable()
+	{
+		m_enabled = false;
+	}
 
-    inline bool GetHookState()
-    {
-        return m_enabled;
-    }
+	inline bool GetHookState()
+	{
+		return m_enabled;
+	}
 
-    inline GUID GetProductGUID()
-    {
-        return m_productid;
-    }
+	inline GUID GetProductGUID()
+	{
+		return m_productid;
+	}
 
-    inline GUID GetInstanceGUID()
-    {
-        return m_instanceid;
-    }
+	inline GUID GetInstanceGUID()
+	{
+		return m_instanceid;
+	}
 
-    inline DWORD GetProductPIDVID()
-    {
-        return m_productid.Data1;
-    }
+	inline DWORD GetProductPIDVID()
+	{
+		return m_productid.Data1;
+	}
 
 	inline DWORD GetUserIndex()
 	{
@@ -74,9 +74,9 @@ public:
 	}
 
 private:
-    bool  m_enabled;
-    GUID  m_productid;
-    GUID  m_instanceid;
+	bool  m_enabled;
+	GUID  m_productid;
+	GUID  m_instanceid;
 	DWORD m_userindex;
 };
 
@@ -86,85 +86,88 @@ private:
 	recursive_mutex m_mutex;
 
 public:
-    iHook(HMODULE instance)
-        :m_hookmask(0x80000000)
-        ,m_fakepidvid(MAKELONG(0x045E,0x028E))
-        ,m_mod(instance)
-		,m_timeout(30)
-    {
-    }
-    virtual ~iHook()
-    {
+	iHook(HMODULE instance)
+		:m_hookmask(0x80000000)
+		, m_fakepidvid(MAKELONG(0x045E, 0x028E))
+		, m_mod(instance)
+		, m_timeout(30)
+	{
+	}
+	virtual ~iHook()
+	{
 		lock_guard lock(m_mutex);
-        MH_Uninitialize();
-        m_devices.clear();
-    };
+		MH_Uninitialize();
+		m_devices.clear();
 
-	static const DWORD HOOK_NONE		= (DWORD)0;
-	static const DWORD HOOK_LL			= (DWORD)1;
-	static const DWORD HOOK_COM			= (DWORD)1 << 1;
-	static const DWORD HOOK_DI			= (DWORD)1 << 2;
-	static const DWORD HOOK_PIDVID		= (DWORD)1 << 3;
-	static const DWORD HOOK_NAME		= (DWORD)1 << 4;
-	static const DWORD HOOK_SA			= (DWORD)1 << 5;
+		if (m_timeout_thread) CloseHandle(m_timeout_thread);
+	};
 
-	static const DWORD HOOK_WT			= (DWORD)1 << 24;
-	static const DWORD HOOK_STOP		= (DWORD)1 << 25;
-	static const DWORD HOOK_DISABLE		= (DWORD)1 << 31;
+	static const DWORD HOOK_NONE		= (DWORD)0;			// 0x00000000
+	static const DWORD HOOK_LL			= (DWORD)1;			// 0x00000001
+	static const DWORD HOOK_COM			= (DWORD)1 << 1;	// 0x00000002
+	static const DWORD HOOK_DI			= (DWORD)1 << 2;	// 0x00000004
+	static const DWORD HOOK_PIDVID		= (DWORD)1 << 3;	// 0x00000008
+	static const DWORD HOOK_NAME		= (DWORD)1 << 4;	// 0x00000010
+	static const DWORD HOOK_SA			= (DWORD)1 << 5;	// 0x00000020
+
+	static const DWORD HOOK_WT			= (DWORD)1 << 24;	// 0x10000000
+	static const DWORD HOOK_STOP		= (DWORD)1 << 25;	// 0x20000000
+	static const DWORD HOOK_NOTIMEOUT	= (DWORD)1 << 26;	// 0x40000000
+	static const DWORD HOOK_DISABLE		= (DWORD)1 << 31;	// 0x80000000
 
 	typedef std::vector<iHookDevice>::iterator iterator;
-    typedef std::vector<iHookDevice>::const_iterator const_iterator;
+	typedef std::vector<iHookDevice>::const_iterator const_iterator;
 
-    iterator begin() { return m_devices.begin(); }
-    const_iterator cbegin() const { return m_devices.cbegin(); }
-    iterator end() { return m_devices.end(); }
-    const_iterator cend() const { return m_devices.cend(); }
+	iterator begin() { return m_devices.begin(); }
+	const_iterator cbegin() const { return m_devices.cbegin(); }
+	iterator end() { return m_devices.end(); }
+	const_iterator cend() const { return m_devices.cend(); }
 
-    inline void Enable()
-    {
-        m_hookmask &= ~HOOK_DISABLE;
-    }
+	inline void Enable()
+	{
+		m_hookmask &= ~HOOK_DISABLE;
+	}
 
-    inline void Disable()
-    {
-        m_hookmask |= HOOK_DISABLE;
-    }
+	inline void Disable()
+	{
+		m_hookmask |= HOOK_DISABLE;
+	}
 
 	inline void EnableHook(const DWORD& flag)
-    {
-        m_hookmask |= flag;
-    }
+	{
+		m_hookmask |= flag;
+	}
 
 	inline void DisableHook(const DWORD& flag)
-    {
-        m_hookmask &= ~flag;
-    }
+	{
+		m_hookmask &= ~flag;
+	}
 
 	inline const bool GetState(const DWORD& flag = HOOK_NONE) const
-    {
-        if (m_hookmask & HOOK_DISABLE || m_hookmask == HOOK_NONE) return false;
-        return (m_hookmask & flag) == flag;
-    }
+	{
+		if (m_hookmask & HOOK_DISABLE || m_hookmask == HOOK_NONE) return false;
+		return (m_hookmask & flag) == flag;
+	}
 
-    inline DWORD GetMask()
-    {
-        return m_hookmask;
-    }
+	inline DWORD GetMask()
+	{
+		return m_hookmask;
+	}
 
 	inline void SetMask(const DWORD& mask)
-    {
-        m_hookmask = mask;
-    }
+	{
+		m_hookmask = mask;
+	}
 
-    inline void SetFakePIDVID(const DWORD& pidvid)
-    {
-        m_fakepidvid = pidvid;
-    }
+	inline void SetFakePIDVID(const DWORD& pidvid)
+	{
+		m_fakepidvid = pidvid;
+	}
 
-    inline DWORD GetFakePIDVID()
-    {
-        return m_fakepidvid;
-    }
+	inline DWORD GetFakePIDVID()
+	{
+		return m_fakepidvid;
+	}
 
 	inline void SetTimeout(const DWORD& timeout)
 	{
@@ -172,89 +175,89 @@ public:
 	}
 
 	inline iHookDevice& GetPadConfig(const DWORD& dwUserIndex)
-    {
-        return m_devices.at(dwUserIndex);
-    }
+	{
+		return m_devices.at(dwUserIndex);
+	}
 
 #if _MSC_VER < 1700
 	inline void AddHook(DWORD userindex, const GUID& productid, const GUID& instanceid)
-    {
-        iHookDevice hdevice(userindex, productid, instanceid);
-        m_devices.push_back(hdevice);
-    }
+	{
+		iHookDevice hdevice(userindex, productid, instanceid);
+		m_devices.push_back(hdevice);
+	}
 #else
 	inline void AddHook(DWORD userindex, const GUID& productid, const GUID& instanceid)
-    {
-        m_devices.emplace_back(userindex, productid, instanceid);
-    }
+	{
+		m_devices.emplace_back(userindex, productid, instanceid);
+	}
 #endif
 
-    inline HMODULE GetEmulator()
-    {
-        return m_mod;
-    }
+	inline HMODULE GetEmulator()
+	{
+		return m_mod;
+	}
 
 	inline static DWORD WINAPI ThreadProc(_In_  LPVOID lpParameter)
 	{
 		DWORD* pTimeout = reinterpret_cast<DWORD*>(lpParameter);
 		if (!pTimeout) return 0;
-		
+
 		Sleep(*pTimeout * 1000);
 		MH_Uninitialize();
 
 		ExitThread(0);
 	}
 
-    inline void ExecuteHooks()
-    {
-        if(!GetState()) 
-        {
-            m_devices.clear();
-            return;
-        }
+	inline void ExecuteHooks()
+	{
+		if (!GetState())
+		{
+			m_devices.clear();
+			return;
+		}
 
 		lock_guard lock(m_mutex);
 
-        PrintLog(LOG_IHOOK,"InputHook starting with mask 0x%08X",m_hookmask);
+		PrintLog(LOG_IHOOK, "InputHook starting with mask 0x%08X", m_hookmask);
 
-        MH_Initialize();
+		MH_Initialize();
 
-        if(GetState(HOOK_LL))
-            HookLL();
+		if (GetState(HOOK_LL))
+			HookLL();
 
-        if(GetState(HOOK_COM))
-            HookCOM();
+		if (GetState(HOOK_COM))
+			HookCOM();
 
-        if(GetState(HOOK_DI))
-            HookDI();
+		if (GetState(HOOK_DI))
+			HookDI();
 
-        if(GetState(HOOK_SA))
-            HookSA();
+		if (GetState(HOOK_SA))
+			HookSA();
 
-        if(GetState(HOOK_WT))
-            HookWT();
+		if (GetState(HOOK_WT))
+			HookWT();
 
-        MH_EnableHook(MH_ALL_HOOKS);
+		MH_EnableHook(MH_ALL_HOOKS);
 
-		if (m_timeout > 0) m_timeout_thread = CreateThread(NULL, NULL, ThreadProc, (LPVOID*) &m_timeout, NULL, NULL);
-    }
+		if (m_timeout > 0 && !GetState(HOOK_NOTIMEOUT)) m_timeout_thread = CreateThread(NULL, NULL, ThreadProc, (LPVOID*)&m_timeout, NULL, NULL);
+	}
 
-    void HookDICOM(REFIID riidltf, LPVOID *ppv);
+	void HookDICOM(REFIID riidltf, LPVOID *ppv);
 
 private:
-    DWORD m_hookmask;
-    DWORD m_fakepidvid;
-    HMODULE m_mod;
+	DWORD m_hookmask;
+	DWORD m_fakepidvid;
+	HMODULE m_mod;
 	DWORD m_timeout;
 	HANDLE m_timeout_thread;
 
-    std::vector<iHookDevice> m_devices;
+	std::vector<iHookDevice> m_devices;
 
-    void HookLL();
-    void HookCOM();
-    void HookDI();
-    void HookWT();
-    void HookSA();
+	void HookLL();
+	void HookCOM();
+	void HookDI();
+	void HookWT();
+	void HookSA();
 };
 
 #endif
