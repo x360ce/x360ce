d11d05a947480bc5b84887a1afde1c30299944b8
ejocys@gmail.com <ejocys@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2014-10-22 16:20:06 +0000

    Started Linearity settings implementation.

---
+++ trunk/x360ce.App/Controls/LinearUserControl.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace x360ce.App.Controls
+{
+	partial class LinearUserControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.LeftTriggerDeadZoneYLabel = new System.Windows.Forms.Label();
+			this.LinearTrackBar = new System.Windows.Forms.TrackBar();
+			this.LeftThumbDeadZoneYTextBox = new System.Windows.Forms.TextBox();
+			this.LinearPictureBox = new System.Windows.Forms.PictureBox();
+			this.TestButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.LinearTrackBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.LinearPictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// LeftTriggerDeadZoneYLabel
+			// 
+			this.LeftTriggerDeadZoneYLabel.AutoSize = true;
+			this.LeftTriggerDeadZoneYLabel.Location = new System.Drawing.Point(12, 36);
+			this.LeftTriggerDeadZoneYLabel.Name = "LeftTriggerDeadZoneYLabel";
+			this.LeftTriggerDeadZoneYLabel.Size = new System.Drawing.Size(17, 13);
+			this.LeftTriggerDeadZoneYLabel.TabIndex = 7;
+			this.LeftTriggerDeadZoneYLabel.Text = "Y:";
+			// 
+			// LinearTrackBar
+			// 
+			this.LinearTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.LinearTrackBar.AutoSize = false;
+			this.LinearTrackBar.Location = new System.Drawing.Point(1, 137);
+			this.LinearTrackBar.Maximum = 100;
+			this.LinearTrackBar.Minimum = -100;
+			this.LinearTrackBar.Name = "LinearTrackBar";
+			this.LinearTrackBar.Size = new System.Drawing.Size(227, 32);
+			this.LinearTrackBar.TabIndex = 9;
+			this.LinearTrackBar.TickFrequency = 2;
+			// 
+			// LeftThumbDeadZoneYTextBox
+			// 
+			this.LeftThumbDeadZoneYTextBox.Location = new System.Drawing.Point(35, 33);
+			this.LeftThumbDeadZoneYTextBox.Name = "LeftThumbDeadZoneYTextBox";
+			this.LeftThumbDeadZoneYTextBox.ReadOnly = true;
+			this.LeftThumbDeadZoneYTextBox.Size = new System.Drawing.Size(43, 20);
+			this.LeftThumbDeadZoneYTextBox.TabIndex = 8;
+			this.LeftThumbDeadZoneYTextBox.TabStop = false;
+			this.LeftThumbDeadZoneYTextBox.Text = "0 % ";
+			this.LeftThumbDeadZoneYTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// LinearPictureBox
+			// 
+			this.LinearPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.LinearPictureBox.BackColor = System.Drawing.Color.White;
+			this.LinearPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.LinearPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.LinearPictureBox.Location = new System.Drawing.Point(98, 3);
+			this.LinearPictureBox.Name = "LinearPictureBox";
+			this.LinearPictureBox.Size = new System.Drawing.Size(130, 130);
+			this.LinearPictureBox.TabIndex = 10;
+			this.LinearPictureBox.TabStop = false;
+			// 
+			// TestButton
+			// 
+			this.TestButton.Location = new System.Drawing.Point(4, 4);
+			this.TestButton.Name = "TestButton";
+			this.TestButton.Size = new System.Drawing.Size(75, 23);
+			this.TestButton.TabIndex = 11;
+			this.TestButton.Text = "Test";
+			this.TestButton.UseVisualStyleBackColor = true;
+			this.TestButton.Click += new System.EventHandler(this.TestButton_Click);
+			// 
+			// LinearUserControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.TestButton);
+			this.Controls.Add(this.LinearPictureBox);
+			this.Controls.Add(this.LeftTriggerDeadZoneYLabel);
+			this.Controls.Add(this.LinearTrackBar);
+			this.Controls.Add(this.LeftThumbDeadZoneYTextBox);
+			this.Name = "LinearUserControl";
+			this.Size = new System.Drawing.Size(231, 179);
+			((System.ComponentModel.ISupportInitialize)(this.LinearTrackBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.LinearPictureBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label LeftTriggerDeadZoneYLabel;
+		private System.Windows.Forms.TrackBar LinearTrackBar;
+		private System.Windows.Forms.TextBox LeftThumbDeadZoneYTextBox;
+		private System.Windows.Forms.PictureBox LinearPictureBox;
+		private System.Windows.Forms.Button TestButton;
+	}
+}
---
+++ trunk/x360ce.App/Controls/LinearUserControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace x360ce.App.Controls
+{
+	public partial class LinearUserControl : UserControl
+	{
+		public LinearUserControl()
+		{
+			InitializeComponent();
+		}
+
+		private void TestButton_Click(object sender, EventArgs e)
+		{
+			var bmp = new Bitmap(128, 128);
+			LinearPictureBox.BackgroundImage = bmp;
+			var g = Graphics.FromImage(bmp);
+			var nativeBrush = new SolidBrush(System.Drawing.Color.Gray);
+			var transBrush = new SolidBrush(System.Drawing.Color.Red);
+			var w = bmp.Width;
+			for (int i = 0; i < w; i++)
+			{
+				g.FillEllipse(nativeBrush, i - 1, w - i - 1, 2, 2);
+				g.FillEllipse(transBrush, i - 1, w - (GetValue(i * 256) / 256) - 1, 2, 2);
+			}
+		}
+
+		int GetValue(int v)
+		{
+			short absval = (short)((Math.Abs(v) + (((32767.0 / 2.0) - (((Math.Abs((Math.Abs(v)) - (32767.0 / 2.0)))))) * (v * 0.01))));
+			v = v > 0 ? absval : -absval;
+			return v;
+		}
+	}
+}
