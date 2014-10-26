06c795e34cee7d2a18cee67dfdfe61cced20a7b4
Evaldas Jocys <ejocys@users.noreply.github.com>
2014-10-26 19:59:27 +0000

    Web service fixed. Auto-calculated columns Vendor ID and Product ID added to database.

--- x360ce.Data/dbo/Tables/x360ce_Products.sql
+++ x360ce.Data/dbo/Tables/x360ce_Products.sql
@@ -2,8 +2,12 @@
     [ProductGuid]   UNIQUEIDENTIFIER NOT NULL,
     [ProductName]   NVARCHAR (256)   NOT NULL,
     [InstanceCount] INT              CONSTRAINT [DF_x360ce_Products_InstanceCount] DEFAULT ((0)) NOT NULL,
+    [VendorId]      AS               (isnull(CONVERT([int],CONVERT([varbinary](4),substring(CONVERT([varchar](36),[ProductGuid],(0)),(5),(4)),(2)),(0)),(0))) PERSISTED NOT NULL,
+    [ProductId]     AS               (isnull(CONVERT([int],CONVERT([varbinary](4),substring(CONVERT([varchar](36),[ProductGuid],(0)),(1),(4)),(2)),(0)),(0))) PERSISTED NOT NULL,
     CONSTRAINT [PK_x360ce_Products] PRIMARY KEY CLUSTERED ([ProductGuid] ASC)
 );
+
+
 
 
 GO
