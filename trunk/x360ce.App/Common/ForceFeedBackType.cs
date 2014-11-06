4c6e603918d37d979ff74e318a7086f32567df7f
ejocys@gmail.com <ejocys@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2014-11-06 08:27:32 +0000

    ForceFeedBackType names fixed: Constant = 0, PeriodicSine = 1, PeriodicSawtooth = 2,

--- trunk/x360ce.App/Common/ForceFeedBackType.cs
+++ trunk/x360ce.App/Common/ForceFeedBackType.cs
@@ -3,10 +3,10 @@
 	public enum ForceFeedBackType
 	{
 		/// <summary>Old constant force (like in 3.1.4.1)</summary>
-		Default = 0,
+		Constant = 0,
 		/// <summary>EJocys method (from rev 150)</summary>
-		PeriodicForWheels = 1,
-		/// <summary>New constant force</summary>
-		Constant = 2,
+		PeriodicSine = 1,
+		/// <summary>New force</summary>
+		PeriodicSawtooth = 2,
 	}
 }
