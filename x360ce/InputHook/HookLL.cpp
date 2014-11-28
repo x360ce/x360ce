8e5ee600804f3d12882ebdde80c83323a4377e3f
tapcio@gmail.com <tapcio@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2014-11-28 11:58:08 +0000

    Update copyrights. Change ditool project to v120_xp toolset, and updated ditool.exe in Support files. Remove some old files from Support.

--- x360ce/InputHook/HookLL.cpp
+++ x360ce/InputHook/HookLL.cpp
@@ -1,22 +1,3 @@
-/*  x360ce - XBOX360 Controller Emulator
-*
-*  https://code.google.com/p/x360ce/
-*
-*  Copyright (C) 2002-2010 Racer_S
-*  Copyright (C) 2010-2013 Robert Krawczyk
-*
-*  x360ce is free software: you can redistribute it and/or modify it under the terms
-*  of the GNU Lesser General Public License as published by the Free Software Foundation,
-*  either version 3 of the License, or any later version.
-*
-*  x360ce is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-*  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-*  PURPOSE.  See the GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License along with x360ce.
-*  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "stdafx.h"
 #include "globals.h"
 #include "Misc.h"
--- x360ce/InputHook/InputHook.h
+++ x360ce/InputHook/InputHook.h
@@ -1,19 +1,18 @@
-/*  x360ce - XBOX360 Controller Emulator
+/*  InputHook Detour Library for XBOX360 Controller Emulator
  *
  *  https://code.google.com/p/x360ce/
  *
- *  Copyright (C) 2002-2010 Racer_S
- *  Copyright (C) 2010-2013 Robert Krawczyk
+ *  Copyright (C) 2012-2014 Robert Krawczyk
  *
- *  x360ce is free software: you can redistribute it and/or modify it under the terms
+ *  InputHook is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Foundation,
  *  either version 3 of the License, or any later version.
  *
- *  x360ce is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  InputHook is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  *  PURPOSE.  See the GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along with x360ce.
+ *  You should have received a copy of the GNU General Public License along with InputHook.
  *  If not, see <http://www.gnu.org/licenses/>.
  */
 
