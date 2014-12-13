a3353243b89f97c0847a7b9c21283980267a086d
ejocys@gmail.com <ejocys@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2014-12-13 14:08:07 +0000

    Updates application Build settings

---
+++ trunk/x360ce.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("TocaEdit X360 Controller Emulator Database")]
+[assembly: AssemblyDescription("SQL Databse for TocaEdit X360 Controller Emulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TocaEdit")]
+[assembly: AssemblyProduct("X360 Controller Emulator Database")]
+[assembly: AssemblyCopyright("Copyright © TocaEdit 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Resources.NeutralResourcesLanguageAttribute("en")]
