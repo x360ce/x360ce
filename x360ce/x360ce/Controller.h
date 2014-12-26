29e02e52575b4582218cc7ee1bd0052e068432a3
tapcio@gmail.com <tapcio@gmail.com@eee1bee7-9acd-8ec4-d849-8c420a588be7>
2014-12-26 13:29:10 +0000

    x360ce: Controller copy constructor is required to initialize ForceFeedback object.

--- x360ce/x360ce/Controller.h
+++ x360ce/x360ce/Controller.h
@@ -13,6 +13,22 @@ public:
     Controller(u32 user);
     ~Controller();
 
+    Controller(const Controller &other):
+        m_ForceFeedback(this)
+    {
+        m_pDevice = other.m_pDevice;
+        m_state = other.m_state;
+        m_mapping = other.m_mapping;
+        m_productid = other.m_productid;
+        m_instanceid = other.m_instanceid;
+        m_user = other.m_user;
+        m_axiscount = other.m_axiscount;
+        m_gamepadtype = other.m_gamepadtype;
+        m_passthrough = other.m_passthrough;
+        m_useforce = other.m_useforce;
+        m_failcount = other.m_failcount;
+    }
+
     DWORD GetState(XINPUT_STATE* pState);
     DWORD CreateDevice();
 
