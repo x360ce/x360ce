3e0f626576033c3c3e9139b805bb6e63827fab59
Evaldas Jocys <ejocys@users.noreply.github.com>
2015-02-27 17:36:04 +0000

    Game Settings User Control updates 6 (work in progress, 64-bit support)

--- x360ce.App/Common/XInputMask.cs
+++ x360ce.App/Common/XInputMask.cs
@@ -6,15 +6,27 @@ namespace x360ce.App
 	public enum XInputMask: uint
 	{
 		None = 0x000,
+		// 32-bit build.
 		[Description("xinput1_1.dll")]
-		Xinput11 = 0x001,
+		XInput11_x86 = 0x01,
 		[Description("xinput1_2.dll")]
-		Xinput12 = 0x002,
+		XInput12_x86 = 0x02,
 		[Description("xinput1_3.dll")]
-		Xinput13 = 0x004,
+		XInput13_x86 = 0x04,
 		[Description("xinput1_4.dll")]
-		Xinput14 = 0x008,
+		XInput14_x86 = 0x08,
 		[Description("xinput9_1_0.dll")]
-		Xinput91 = 0x100,
+		XInput91_x86 = 0x40,
+		// 64-bit build.
+		[Description("xinput1_1.dll")]
+		XInput11_x64 = 0x0100,
+		[Description("xinput1_2.dll")]
+		XInput12_x64 = 0x0200,
+		[Description("xinput1_3.dll")]
+		XInput13_x64 = 0x0400,
+		[Description("xinput1_4.dll")]
+		XInput14_x64 = 0x0800,
+		[Description("xinput9_1_0.dll")]
+		XInput91_x64 = 0x4000,
 	}
 }
