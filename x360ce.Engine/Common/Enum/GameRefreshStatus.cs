83b52b64dc0785da453b666ffc55c7f121ed139a
Evaldas Jocys <ejocys@users.noreply.github.com>
2015-03-13 17:06:30 +0000

    Updates on Game Settings: Check and synchronize function updates 2 (work in progress)

--- x360ce.Engine/Common/Enum/GameRefreshStatus.cs
+++ x360ce.Engine/Common/Enum/GameRefreshStatus.cs
@@ -1,19 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 
 namespace x360ce.Engine
 {
+	[Flags]
 	public enum GameRefreshStatus
 	{
-		None = 0,
+		[Description("All is OK")]
 		OK = 0x1,
-		FileNotExist = 0x2,
-		XInputFileNotExist = 0x4,
-		XInputFileWrongPlatform = 0x8,
-		XInputFileOlderVersion = 0x10,
-		XInputFileNewerVersion = 0x20,
-		XInputFileUnnecessary = 0x40,
+		[Description("Executable was not found.")]
+		ExeNotExist = 0x2,
+		[Description("Game Database file not found.")]
+		GdbNotExist = 0x4,
+		[Description("Game Database file is different.")]
+		GdbDifferent = 0x8,
+		[Description("INI settings file not found.")]
+		IniNotExist = 0x10,
+		[Description("INI settings file is different.")]
+		IniDifferent = 0x20,
+		[Description("XML settings file not found.")]
+		XmlNotExist = 0x40,
+		[Description("XML settings file is different.")]
+		XmlDifferent = 0x80,
+		[Description("XInput files are missing.")]
+		XInputFilesNotExist = 0x100,
+		[Description("XInput files are for different processor arhitecture.")]
+		XInputFilesWrongPlatform = 0x200,
+		[Description("Older version of XInput files were found.")]
+		XInputFilesOlderVersion = 0x400,
+		[Description("Newer version of XInput files were found.")]
+		XInputFilesNewerVersion = 0x800,
+		[Description("Unecessary XInput files were found.")]
+		XInputFilesUnnecessary = 0x1000,
 	}
 }
