240d4d7de67714110dcea9e7adaf8370f7378428
Evaldas Jocys <ejocys@users.noreply.github.com>
2015-04-19 15:38:16 +0000

    Version and changelog updated.

--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.1.3")]
-[assembly: AssemblyFileVersionAttribute("3.0.1.3")]
+[assembly: AssemblyVersion("3.0.1.6")]
+[assembly: AssemblyFileVersionAttribute("3.0.1.6")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
