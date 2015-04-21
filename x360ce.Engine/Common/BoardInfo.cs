f74b8e59b29d431a1f54db3fc28672ee3caa464e
Evaldas Jocys <evaldas@jocys.com>
2015-04-21 20:42:27 +0100

    GetDiskDriveID crash fix

--- x360ce.Engine/Common/BoardInfo.cs
+++ x360ce.Engine/Common/BoardInfo.cs
@@ -1,65 +1,68 @@
 ﻿namespace x360ce.Engine
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Management;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Management;
 
-	public class BoardInfo
-	{
-		/// <summary>
-		/// Get manufacturer-allocated number used to identify the physical element.
-		/// http://msdn.microsoft.com/en-us/library/aa394132%28VS.85%29.aspx
-		/// </summary>
-		static string GetDiskDriveID()
-		{
-			return GetClassValue<string>("Win32_DiskDrive", "SerialNumber");
-		}
+    public class BoardInfo
+    {
+        /// <summary>
+        /// Get manufacturer-allocated number used to identify the physical element.
+        /// http://msdn.microsoft.com/en-us/library/aa394132%28VS.85%29.aspx
+        /// </summary>
+        static string GetDiskDriveID()
+        {
+            string serialNumber = null;
+            try
+            {
+                lock (searcherLock)
+                {
+                    var query = "SELECT SerialNumber, Index, MediaType FROM Win32_DiskDrive";
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                    ManagementObjectCollection moc = searcher.Get();
+                    var items = moc.Cast<ManagementObject>().Select(x => new
+                    {
+                        Index = (uint)x["Index"],
+                        MediaType = ((string)x["MediaType"] ?? "").Trim().ToLower(),
+                        SerialNumber = (string)x["SerialNumber"],
 
-		static Guid? _DiskDriveIdGuid;
+                    });
+                    // Get number of first hard drive.
+                    var item = items.FirstOrDefault(x => !string.IsNullOrEmpty(x.SerialNumber) && x.MediaType.Contains("fixed"));
+                    // if not found then get any non empty.
+                    if (item == null) item = items.FirstOrDefault(x => !string.IsNullOrEmpty(x.SerialNumber));
+                    moc.Dispose();
+                    searcher.Dispose();
+                    if (item != null) serialNumber = item.SerialNumber;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("BoardInfo Win32_DiskDrive SerialNumber read failed: {0}", ex.Message));
+            }
+            return serialNumber;
+        }
 
-		/// <summary>
-		/// This anonymous guid will be used as link betwen disk and game settings on online database.
-		/// </summary>
-		public static Guid GetDiskDriveIdGuid()
-		{
-			if (_DiskDriveIdGuid.HasValue) return _DiskDriveIdGuid.Value;
+        static Guid? _DiskDriveIdGuid;
+
+        /// <summary>
+        /// This anonymous guid will be used as link betwen disk and game settings on online database.
+        /// </summary>
+        public static Guid GetDiskDriveIdGuid()
+        {
+            if (_DiskDriveIdGuid.HasValue) return _DiskDriveIdGuid.Value;
             var diskId = GetDiskDriveID();
             var serialBytes = System.Text.Encoding.ASCII.GetBytes(diskId);
-			var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
-			byte[] retVal = md5.ComputeHash(serialBytes);
-			_DiskDriveIdGuid = new Guid(retVal);
-			return _DiskDriveIdGuid.Value;
-		}
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(serialBytes);
+            _DiskDriveIdGuid = new Guid(retVal);
+            return _DiskDriveIdGuid.Value;
+        }
 
-		static object searcherLock = new object();
+        static object searcherLock = new object();
 
-		static T GetClassValue<T>(string className, string paramName)
-		{
-			T value = default(T);
-			try
-			{
-				lock (searcherLock)
-				{
-					var query = string.Format("SELECT {0} FROM {1}", paramName, className);
-					ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
-					ManagementObjectCollection moc = searcher.Get();
-					foreach (ManagementObject mo in moc)
-					{
-						value = (T)mo[paramName];
-					}
-					moc.Dispose();
-					searcher.Dispose();
-				}
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(string.Format("BoardInfo {0} {1} read failed: {2}", className, paramName, ex.Message));
-			}
-			return value;
-		}
-	
-	
-	}
+
+    }
 }
