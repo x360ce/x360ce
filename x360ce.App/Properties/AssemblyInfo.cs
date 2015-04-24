b6b9cdb17b8956f8f1e612abe4f676ce34538884
Evaldas Jocys <evaldas@jocys.com>
2015-04-24 08:37:54 +0100

    Fix: Crash when x360ce.xml settings file is corrupted.

--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.2.14")]
-[assembly: AssemblyFileVersionAttribute("3.0.2.14")]
+[assembly: AssemblyVersion("3.0.2.15")]
+[assembly: AssemblyFileVersionAttribute("3.0.2.15")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
