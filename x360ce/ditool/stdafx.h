21acfbfe601c49f4922eabe44b30137f23856c45
Robert Krawczyk <robkraw@gmail.com>
2015-05-04 18:34:31 +0200

    Remove VS2010 hacks.

--- x360ce/ditool/stdafx.h
+++ x360ce/ditool/stdafx.h
@@ -18,14 +18,6 @@
 
 #include "targetver.h"
 
-#if _MSC_VER < 1700
-#define _BIND_TO_CURRENT_CRT_VERSION 1
-
-#ifdef NDEBUG
-#define _SECURE_SCL 0
-#endif
-#endif
-
 #define WIN32_LEAN_AND_MEAN
 #define STRICT
 
