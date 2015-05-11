25eadcb496ec20c24bc58cb5d8cce24a29a91ded
Evaldas Jocys <evaldas@jocys.com>
2015-05-11 19:13:56 +0100

    Version changed to v3.0.3.34 (2015-05-11). Release to public.

--- x360ce.App/Properties/AssemblyInfo.cs
+++ x360ce.App/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.3.32")]
-[assembly: AssemblyFileVersionAttribute("3.0.3.32")]
+[assembly: AssemblyVersion("3.0.3.34")]
+[assembly: AssemblyFileVersionAttribute("3.0.3.34")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
