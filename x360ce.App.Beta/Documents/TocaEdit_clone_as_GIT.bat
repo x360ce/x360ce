5ed0fb0a6d4188759ea06b0ce148a9878260c4d8
Evaldas Jocys <evaldas@jocys.com>
2016-07-17 13:04:02 +0100

    GIT and SVN checkout scripts updated.

--- x360ce.App.Beta/Documents/TocaEdit_clone_as_GIT.bat
+++ x360ce.App.Beta/Documents/TocaEdit_clone_as_GIT.bat
@@ -1,3 +1,5 @@
+:: TocaEdit_clone_as_GIT.bat
+::-------------------------------------------------------------
 SET prg="%ProgramFiles%\Git\bin\git.exe"
 IF NOT EXIST %prg% SET prg="%ProgramFiles(x86)%\Git\bin\git.exe"
 IF NOT EXIST %prg% SET prg="%ProgramW6432%\Git\bin\git.exe"
