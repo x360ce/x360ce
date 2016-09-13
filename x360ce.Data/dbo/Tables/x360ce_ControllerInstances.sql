a36632374cc4f4fab6749a4085b0b403c35d372f
Evaldas Jocys <evaldas@jocys.com>
2016-09-13 17:13:54 +0100

    Add table to store mappings between devices and instances (helps to configure devices after moving to a new computer)

---
+++ x360ce.Data/dbo/Tables/x360ce_ControllerInstances.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[x360ce_ControllerInstances] (
+    [Id]           UNIQUEIDENTIFIER CONSTRAINT [DF_x360ce_ControllerInstances_Id] DEFAULT (newid()) NOT NULL,
+    [ControllerId] UNIQUEIDENTIFIER NOT NULL,
+    [InstanceGuid] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_x360ce_ControllerInstances] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ x360ce.Data/dbo/Tables/x360ce_Controllers.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[x360ce_Controllers] (
+    [Id]            UNIQUEIDENTIFIER CONSTRAINT [DF_x360ce_Controllers_Id] DEFAULT (newid()) NOT NULL,
+    [ApplicationId] UNIQUEIDENTIFIER NOT NULL,
+    [UserId]        UNIQUEIDENTIFIER NOT NULL,
+    [VendorName]    NVARCHAR (256)   NOT NULL,
+    [ProductName]   NVARCHAR (256)   NOT NULL,
+    [DeviceId]      VARCHAR (512)    NOT NULL,
+    [FriendlyName]  NVARCHAR (256)   NOT NULL,
+    [DateCreated]   DATETIME         CONSTRAINT [DF_x360ce_Controllers_DateCreated] DEFAULT (getdate()) NOT NULL,
+    [DateUpdated]   DATETIME         NOT NULL,
+    [IsEnabled]     BIT              CONSTRAINT [DF_x360ce_Controllers_IsEnabled] DEFAULT ((1)) NOT NULL,
+    [IsOnline]      BIT              CONSTRAINT [DF_x360ce_Controllers_IsOnline] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_x360ce_Controllers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
