7721b049c6b06dded388c747b9b14e503e6b5d62
Evaldas Jocys <evaldas@jocys.com>
2017-10-10 17:22:13 +0100

    State code updated.

---
+++ x360ce.App.Beta/Common/DInput/DInputHelper.Step2.UpdateDiStates.cs
@@ -0,0 +1,47 @@
+﻿using SharpDX.DirectInput;
+using SharpDX.XInput;
+using System;
+using System.Linq;
+using x360ce.Engine;
+
+namespace x360ce.App.DInput
+{
+	public partial class DInputHelper
+	{
+
+		void UpdateDiStates()
+		{
+			// Get all mapped user instances.
+			var instanceGuids = SettingsManager.Settings.Items
+				.Where(x => x.MapTo > (int)MapTo.None)
+				.Select(x => x.InstanceGuid).ToArray();
+			// Get all connected devices.
+			var userDevices = SettingsManager.UserDevices.Items
+				.Where(x => instanceGuids.Contains(x.InstanceGuid) && x.IsOnline)
+				.ToArray();
+			for (int i = 0; i < userDevices.Count(); i++)
+			{
+				var ud = userDevices[i];
+				JoystickState state = null;
+				// Update direct input form and return actions (pressed buttons/dpads, turned axis/sliders).
+				var isOnline = ud != null && ud.IsOnline;
+				var device = ud.Device;
+				if (isOnline && device != null)
+				{
+					try
+					{
+						device.Acquire();
+						state = device.GetCurrentState();
+					}
+					catch (Exception ex)
+					{
+						var error = ex;
+					}
+				}
+				ud.JoState = state ?? new JoystickState();
+				ud.DiState = new CustomDiState(state);
+			}
+		}
+
+	}
+}
--- x360ce.App.Beta/Common/DInput/DInputHelper.Step3.UpdateXiStates.cs
+++ x360ce.App.Beta/Common/DInput/DInputHelper.Step3.UpdateXiStates.cs
@@ -8,41 +8,6 @@ namespace x360ce.App.DInput
 {
 	public partial class DInputHelper
 	{
-
-		void UpdateDiStates()
-		{
-			// Get all mapped user instances.
-			var instanceGuids = SettingsManager.Settings.Items
-				.Where(x => x.MapTo > (int)MapTo.None)
-				.Select(x => x.InstanceGuid).ToArray();
-			// Get all connected devices.
-			var userDevices = SettingsManager.UserDevices.Items
-				.Where(x => instanceGuids.Contains(x.InstanceGuid) && x.IsOnline)
-				.ToArray();
-			for (int i = 0; i < userDevices.Count(); i++)
-			{
-				var ud = userDevices[i];
-				JoystickState state = null;
-				// Update direct input form and return actions (pressed buttons/dpads, turned axis/sliders).
-				var isOnline = ud != null && ud.IsOnline;
-				var device = ud.Device;
-				if (isOnline && device != null)
-				{
-					try
-					{
-						device.Acquire();
-						state = device.GetCurrentState();
-					}
-					catch (Exception ex)
-					{
-						var error = ex;
-					}
-				}
-				ud.JoState = state ?? new JoystickState();
-				ud.DiState = new CustomDiState(state);
-			}
-		}
-
 		/// <summary>
 		/// Convert DiStates to XInput states.
 		/// </summary>
@@ -124,6 +89,5 @@ namespace x360ce.App.DInput
 			if (ev != null)
 				ev(this, new EventArgs());
 		}
-
 	}
 }
