dcdbe8f21b134592ebbb779de9cfd87eb4e40a1e
Evaldas Jocys <evaldas@jocys.com>
2018-02-28 17:10:28 +0000

    ForceFeedback updates.

--- x360ce.App.Beta/Common/DInput/DInputHelper.Step2.UpdateDiStates.cs
+++ x360ce.App.Beta/Common/DInput/DInputHelper.Step2.UpdateDiStates.cs
@@ -95,38 +95,16 @@ namespace x360ce.App.DInput
 		#region Force Feedback
 
 		const int DI_FFNOMINALMAX = 10000;
-		const uint INFINITE = 0xFFFFFFFF;
-		const uint DIEB_NOTRIGGER = 0xFFFFFFFF;
-
-
-		uint m_OveralStrength;
-		byte m_Type;
-		// Left Motor.
-		uint m_LeftPeriod;
-		uint m_LeftStrength;
-		int m_LeftDirection;
-		// Right Motor.
-		uint m_RightPeriod;
-		uint m_RightStrength;
-		int m_RightDirection;
-		bool m_SwapMotors;
 
 		bool SetDeviceForces(UserDevice ud, PadSetting ps, Vibration v)
 		{
-			var actuatorCount = 0;
 			if (ud.FFState == null)
 			{
-				ud.FFState = new Engine.ForceFeedbackState();
-				// Find actuators.
-				var actuators = ud.DeviceObjects.Where(x => x.Flags.HasFlag(DeviceObjectTypeFlags.ForceFeedbackActuator)).ToArray();
-				actuatorCount = actuators.Length;
+				ud.FFState = new Engine.ForceFeedbackState(ud);
 			}
-			// Return if no force feedback actuators found.
-			if (actuatorCount == 0)
+			// Return if force feedback actuators not found.
+			if (ud.FFState.LeftActuator == null)
 				return false;
-
-			var leftSpeed = v.LeftMotorSpeed;
-			var rightSpeed = v.RightMotorSpeed;
 
 			var leftPeriod = int.Parse(ps.LeftMotorDirection) * 1000;
 			var rightPeriod = int.Parse(ps.RightMotorPeriod) * 1000;
@@ -134,11 +112,11 @@ namespace x360ce.App.DInput
 			var rightStrength = int.Parse(ps.RightMotorStrength);
 			var leftDirection = int.Parse(ps.LeftMotorDirection);
 			var rightDirection = int.Parse(ps.RightMotorDirection);
-
 			var overalStrength = int.Parse(ps.ForceOverall);
-
-
 			var forceType = (ForceFeedBackType)int.Parse(ps.ForceType);
+
+			var leftSpeed = v.LeftMotorSpeed;
+			var rightSpeed = v.RightMotorSpeed;
 
 			// Combine strengths into magnitude.
 			var leftMagnitude = MulDiv(leftSpeed, DI_FFNOMINALMAX, ushort.MaxValue);
@@ -147,48 +125,17 @@ namespace x360ce.App.DInput
 			var leftMagnitudeAdjusted = MulDiv(leftMagnitude, leftStrength, DI_FFNOMINALMAX);
 			var rightMagnitudeAdjusted = MulDiv(rightMagnitude, rightStrength, DI_FFNOMINALMAX);
 
-			// Parameters for created effect.
-			var diEffectX = new EffectParameters();
-			// Parameters for created effect.
-			var diEffectY = new EffectParameters();
-
 			// Right-handed Cartesian direction:
 			// x: -1 = left,     1 = right,   0 - no direction
 			// y: -1 = backward, 1 = forward, 0 - no direction
 			// z: -1 = down,     1 = up,      0 - no direction
 
-			var lZeroX = m_LeftDirection;
-			var lZeroY = m_RightDirection;
-			var DIJOFS_X = ud.DeviceObjects.FirstOrDefault(x => x.Type == ObjectGuid.XAxis).Offset;
-			var DIJOFS_Y = ud.DeviceObjects.FirstOrDefault(x => x.Type == ObjectGuid.YAxis).Offset;
-			// Left motor.
-			diEffectX.Flags = EffectFlags.Cartesian | EffectFlags.ObjectOffsets;
-			diEffectX.Envelope = new Envelope();
-			diEffectX.StartDelay = 0;
-			diEffectX.Duration = unchecked((int)INFINITE);
-			diEffectX.SamplePeriod = 0;
-			diEffectX.Gain = overalStrength;
-			diEffectX.TriggerButton = unchecked((int)DIEB_NOTRIGGER);
-			diEffectX.TriggerRepeatInterval = 0;
-			diEffectY.Axes = new int[1] { DIJOFS_X };
-			diEffectY.Directions = new int[1] { lZeroX };
-			if (actuatorCount > 1)
-			{
-				// Right motor.
-				diEffectY.Axes = new int[1] { DIJOFS_Y };
-				diEffectY.Directions = new int[1] { lZeroY };
-				diEffectY.Flags = EffectFlags.Cartesian | EffectFlags.ObjectOffsets;
-				diEffectY.Envelope = new Envelope();
-				diEffectY.StartDelay = 0;
-				diEffectY.Duration = unchecked((int)INFINITE);
-				diEffectY.SamplePeriod = 0;
-				diEffectY.Gain = overalStrength;
-				diEffectY.TriggerButton = unchecked((int)DIEB_NOTRIGGER);
-				diEffectY.TriggerRepeatInterval = 0;
-			}
+			ud.FFState.UpdateLeftParameters(overalStrength, leftDirection);
+			ud.FFState.UpdateRightParameters(overalStrength, rightDirection);
+
 			Guid GUID_Force;
-			// If device have only one force fedback actuator (probably wheel).
-			if (actuatorCount == 1)
+			// If device have only one force feedback actuator (probably wheel).
+			if (!ud.FFState.RightEnabled)
 			{
 				// Forces must be combined.
 				var combinedMagnitudeAdjusted = Math.Max(leftMagnitudeAdjusted, rightMagnitudeAdjusted);
@@ -203,38 +150,37 @@ namespace x360ce.App.DInput
 				leftMagnitudeAdjusted = combinedMagnitudeAdjusted;
 				leftPeriod = combinedPeriod;
 			}
-
-			// 1 - Periodic 'Sine Wave'.
-			// 2 - Periodic 'Sawtooth Down Wave'.
+			// Forces for torque motors (Wheels).
+			// 1 - Periodic 'Sine Wave'. Good for car/plane engine vibration.
+			// 2 - Periodic 'Sawtooth Down Wave'. Good for gun recoil.
 			if (forceType == ForceFeedBackType.PeriodicSine || forceType == ForceFeedBackType.PeriodicSawtooth)
 			{
-				GUID_Force = forceType == ForceFeedBackType.PeriodicSine
-					? EffectGuid.Sine
-					: EffectGuid.SawtoothDown;
+				GUID_Force = forceType == ForceFeedBackType.PeriodicSine ? EffectGuid.Sine : EffectGuid.SawtoothDown;
 				// Left motor.
 				ud.FFState.LeftPeriodicForce.Magnitude = leftMagnitudeAdjusted;
 				ud.FFState.LeftPeriodicForce.Period = leftPeriod;
-				diEffectX.Parameters = ud.FFState.LeftPeriodicForce;
-				if (actuatorCount > 1)
+				//diEffectX.Parameters = ud.FFState.LeftPeriodicForce;
+				if (ud.FFState.RightEnabled)
 				{
 					// Right motor.
 					ud.FFState.RightPeriodicForce.Magnitude = rightMagnitudeAdjusted;
 					ud.FFState.RightPeriodicForce.Period = rightPeriod;
-					diEffectY.Parameters = ud.FFState.RightPeriodicForce;
+					//diEffectY.Parameters = ud.FFState.RightPeriodicForce;
 				}
 			}
-			// Constant.
+			// Forces for vibrating motors (Game pads).
+			// 0 - Constant. Good for vibrating motors.
 			else
 			{
 				GUID_Force = EffectGuid.ConstantForce;
 				// Left motor.
 				ud.FFState.LeftConstantForce.Magnitude = leftMagnitudeAdjusted;
-				diEffectX.Parameters = ud.FFState.LeftConstantForce;
-				if (actuatorCount > 1)
+				//diEffectX.Parameters = ud.FFState.LeftConstantForce;
+				if (ud.FFState.RightEnabled)
 				{
 					// Right motor.
 					ud.FFState.RightConstantForce.Magnitude = rightMagnitudeAdjusted;
-					diEffectY.Parameters = ud.FFState.RightConstantForce;
+					//diEffectY.Parameters = ud.FFState.RightConstantForce;
 				}
 			}
 			//PrintLog("Type %d Axes %d OMag %d LSpeed %d RSpeed %d LMag %d RMag %d LPeriod %d RPeriod", forceType, m_Axes, m_OveralStrength, leftSpeed, rightSpeed, leftMagnitudeAdjusted, rightMagnitudeAdjusted, leftPeriod, rightPeriod);
@@ -245,7 +191,7 @@ namespace x360ce.App.DInput
 				// Left motor.
 				try
 				{
-					ud.FFState.LeftEffect = new Effect(ud.Device, GUID_Force, diEffectX);
+					//ud.FFState.LeftEffect = new Effect(ud.Device, GUID_Force, diEffectX);
 				}
 				catch (Exception ex)
 				{
@@ -254,13 +200,13 @@ namespace x360ce.App.DInput
 				}
 			}
 			// If no effect exists then...
-			if (actuatorCount > 1 && ud.FFState.RightEffect == null)
+			if (ud.FFState.RightEnabled && ud.FFState.RightEffect == null)
 			{
 				ud.FFState.RightRestart = true;
 				// Right motor.
 				try
 				{
-					ud.FFState.RightEffect = new Effect(ud.Device, GUID_Force, diEffectY);
+					//ud.FFState.RightEffect = new Effect(ud.Device, GUID_Force, diEffectY);
 				}
 				catch (Exception ex)
 				{
@@ -277,9 +223,9 @@ namespace x360ce.App.DInput
 			else
 			{
 				// Modify effect.
-				ud.FFState.LeftEffect.SetParameters(diEffectX);
+				//ud.FFState.LeftEffect.SetParameters(diEffectX);
 			}
-			if (actuatorCount > 1)
+			if (ud.FFState.RightEnabled)
 			{
 				// If start new effect then.
 				if (ud.FFState.RightRestart)
@@ -290,9 +236,9 @@ namespace x360ce.App.DInput
 				else
 				{
 					// Modify effect.
-					ud.FFState.RightEffect.SetParameters(diEffectY);
+					//ud.FFState.RightEffect.SetParameters(diEffectY);
 				}
-				// Restart left motorr effect next time if it was stopped.
+				// Restart left motor effect next time if it was stopped.
 				ud.FFState.LeftRestart = (leftSpeed == 0);
 				// Restart right motor effect next time if it was stopped.
 				ud.FFState.RightRestart = (rightSpeed == 0);
--- x360ce.Engine/Common/ForceFeedbackState.cs
+++ x360ce.Engine/Common/ForceFeedbackState.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using x360ce.Engine.Data;
 
 namespace x360ce.Engine
 {
@@ -12,7 +13,7 @@ namespace x360ce.Engine
 	/// </summary>
 	public class ForceFeedbackState
 	{
-		public ForceFeedbackState()
+		public ForceFeedbackState(UserDevice ud)
 		{
 			LeftRestart = true;
 			RightRestart = true;
@@ -20,10 +21,78 @@ namespace x360ce.Engine
 			RightPeriodicForce = new PeriodicForce();
 			LeftConstantForce = new ConstantForce();
 			RightConstantForce = new ConstantForce();
+			// Find and assign actuators.
+			var actuators = ud.DeviceObjects.Where(x => x.Flags.HasFlag(DeviceObjectTypeFlags.ForceFeedbackActuator)).ToList();
+			// If actuator available then...
+			if (actuators.Count > 0)
+			{
+				// Try to find left actuator.
+				var actuator = actuators.FirstOrDefault(x => x.Type == ObjectGuid.XAxis);
+				// If default actuator not found then take default.
+				if (actuator == null)
+					actuator = actuators[0];
+				actuators.Remove(actuator);
+				LeftActuator = actuator;
+				LeftParameters = GetParameters(LeftActuator.Offset);
+				LeftEnabled = true;
+			}
+			// If actuator available then...
+			if (actuators.Count > 0)
+			{
+				// Try to find right actuator.
+				var actuator = actuators.FirstOrDefault(x => x.Type == ObjectGuid.YAxis);
+				// If default actuator not found then take default.
+				if (actuator == null)
+					actuator = actuators[0];
+				actuators.Remove(actuator);
+				RightActuator = actuator;
+				LeftParameters = GetParameters(RightActuator.Offset);
+				RightEnabled = true;
+			}
+		}
+
+		const uint INFINITE = 0xFFFFFFFF;
+		const uint DIEB_NOTRIGGER = 0xFFFFFFFF;
+
+		EffectParameters GetParameters(int offset)
+		{
+			var p = new EffectParameters();
+			// Right-handed Cartesian direction:
+			// x: -1 = left,     1 = right,   0 - no direction
+			// y: -1 = backward, 1 = forward, 0 - no direction
+			// z: -1 = down,     1 = up,      0 - no direction
+			// Left motor.
+			p.Axes = new int[1] { offset };
+			p.Flags = EffectFlags.Cartesian | EffectFlags.ObjectOffsets;
+			p.Envelope = new Envelope();
+			p.StartDelay = 0;
+			p.Duration = unchecked((int)INFINITE);
+			p.SamplePeriod = 0;
+			p.TriggerButton = unchecked((int)DIEB_NOTRIGGER);
+			p.TriggerRepeatInterval = 0;
+			return p;
+		}
+
+		public void UpdateLeftParameters(int strength, int direction)
+		{
+			if (!LeftEnabled)
+				return;
+			LeftParameters.Gain = strength;
+			LeftParameters.Directions = new int[1] { direction };
+		}
+		public void UpdateRightParameters(int strength, int direction)
+		{
+			if (!RightEnabled)
+				return;
+			RightParameters.Gain = strength;
+			RightParameters.Directions = new int[1] { direction };
 		}
 
 		// Left
 
+		public bool LeftEnabled = false;
+		public DeviceObjectItem LeftActuator;
+		public EffectParameters LeftParameters;
 		public Effect LeftEffect;
 		public bool LeftRestart;
 		public PeriodicForce LeftPeriodicForce;
@@ -31,6 +100,9 @@ namespace x360ce.Engine
 
 		// Right
 
+		public bool RightEnabled = false;
+		public DeviceObjectItem RightActuator;
+		public EffectParameters RightParameters;
 		public Effect RightEffect;
 		public bool RightRestart;
 		public PeriodicForce RightPeriodicForce;
