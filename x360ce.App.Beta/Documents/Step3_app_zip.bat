4a08bc2cbc6ea3953239233b2425de2426d70a1f
Evaldas Jocys <evaldas@jocys.com>
2018-05-12 12:43:29 +0100

    Updated debug information settings and scripts.

--- x360ce.App.Beta/Documents/Step3_app_zip.bat
+++ x360ce.App.Beta/Documents/Step3_app_zip.bat
@@ -4,31 +4,38 @@ if NOT EXIST %wra% SET wra="%ProgramFiles(x86)%\WinRAR\winrar.exe"
 if NOT EXIST %wra% SET wra="%ProgramW6432%\WinRAR\winrar.exe"
 SET zip=%wra% a -ep
 :: ---------------------------------------------------------------------------
+SET x86r=Files\x360ce.zip
+SET x86d=Files\x360ce_debug.zip
+SET x64r=Files\x360ce_x64.zip
+SET x64d=Files\x360ce_x64_debug.zip
 echo --- Delete files
 IF NOT EXIST Files\nul MKDIR Files
-IF EXIST Files\x360ce.zip DEL x360ce.zip
-IF EXIST Files\x360ce_debug.zip DEL x360ce_debug.zip
-IF EXIST Files\x360ce_x64.zip DEL x360ce_x64.zip
-IF EXIST Files\x360ce_x64_debug.zip DEL x360ce_x64_debug.zip
+IF EXIST %x86r% DEL %x86r%
+IF EXIST %x86d% DEL %x86d%
+IF EXIST %x64r% DEL %x64r%
+IF EXIST %x64d% DEL %x64d%
+::-------------------------------------------------------------
 :: Archive x86 application.
-%zip% Files\x360ce.zip "..\bin\Debug_x86\x360ce.exe"
+%zip% %x86r% ..\bin\Debug_x86\x360ce.exe
 :: Archive x86 application with debug info.
-%zip% Files\x360ce_debug.zip "..\bin\Debug_x86\x360ce.exe"
-::%zip% Files\x360ce_debug.zip "..\bin\Debug_x86\x360ce.pdb"
-%zip% Files\x360ce_debug.zip "..\..\x360ce.Engine\bin\Debug_x86\x360ce.Engine.dll"
-::%zip% Files\x360ce_debug.zip "..\..\x360ce.Engine\bin\Debug_x86\x360ce.Engine.pdb"
-%zip% Files\x360ce_debug.zip "..\..\x360ce\x360ce\bin\Debug\xinput1_3.dll"
-%zip% Files\x360ce_debug.zip "..\..\x360ce\x360ce\bin\Debug\xinput1_3.pdb"
-:: Rename x64 application.
-copy /Y "..\bin\Debug_x64\x360ce.exe" "..\bin\Debug_x64\x360ce_x64.exe"
-::copy /Y "..\bin\Debug_x64\x360ce.pdb" "..\bin\Debug_x64\x360ce_x64.pdb"
+%zip% %x86d% ..\bin\Debug_x86\x360ce.exe
+%zip% %x86d% ..\bin\Debug_x86\x360ce.pdb
+%zip% %x86d% ..\bin\Debug_x86\x360ce.Engine.dll
+%zip% %x86d% ..\bin\Debug_x86\x360ce.Engine.pdb
+%zip% %x86d% ..\..\x360ce\x360ce\bin\Debug\xinput1_3.dll
+%zip% %x86d% ..\..\x360ce\x360ce\bin\Debug\xinput1_3.pdb
+::-------------------------------------------------------------
+:: Rename 64-bit file so it can be in same folder as 32-bit exe.
+copy /Y ..\bin\Debug_x64\x360ce.exe ..\bin\Debug_x64\x360ce_x64.exe
+copy /Y ..\bin\Debug_x64\x360ce.pdb ..\bin\Debug_x64\x360ce_x64.pdb
+::-------------------------------------------------------------
 :: Archive x64 application.
-%zip% Files\x360ce_x64.zip "..\bin\Debug_x64\x360ce_x64.exe"
+%zip% %x64r% ..\bin\Debug_x64\x360ce_x64.exe
 :: Archive x64 application with debug info.
-%zip% Files\x360ce_x64_debug.zip "..\bin\Debug_x64\x360ce_x64.exe"
-::%zip% Files\x360ce_x64_debug.zip "..\bin\Debug_x64\x360ce_x64.pdb"
-%zip% Files\x360ce_x64_debug.zip "..\..\x360ce.Engine\bin\Debug_x64\x360ce.Engine.dll"
-::%zip% Files\x360ce_x64_debug.zip "..\..\x360ce.Engine\bin\Debug_x64\x360ce.Engine.pdb"
-%zip% Files\x360ce_x64_debug.zip "..\..\x360ce\x360ce\bin64\Debug\xinput1_3.dll"
-%zip% Files\x360ce_x64_debug.zip "..\..\x360ce\x360ce\bin64\Debug\xinput1_3.pdb"
+%zip% %x64d% ..\bin\Debug_x64\x360ce_x64.exe
+%zip% %x64d% ..\bin\Debug_x64\x360ce_x64.pdb
+%zip% %x64d% ..\bin\Debug_x64\x360ce.Engine.dll
+%zip% %x64d% ..\bin\Debug_x64\x360ce.Engine.pdb
+%zip% %x64d% ..\..\x360ce\x360ce\bin64\Debug\xinput1_3.dll
+%zip% %x64d% ..\..\x360ce\x360ce\bin64\Debug\xinput1_3.pdb
 pause
