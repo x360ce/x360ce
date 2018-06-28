407824cf802bbda95a531d577ee1b71809d3e2a4
Evaldas Jocys <evaldas@jocys.com>
2018-06-28 17:31:11 +0100

    Fix polling rate enumeration values.

--- x360ce.App.Beta/Common/UpdateFrequency.cs
+++ x360ce.App.Beta/Common/UpdateFrequency.cs
@@ -6,11 +6,11 @@ namespace x360ce.App
 	{
 		ms1_1000Hz = 1,
 		ms2_500Hz = 2,
-		//ms3_333Hz = 2,
+		//ms3_333Hz = 3,
 		ms4_250Hz = 4,
-		//ms5_200Hz = 4,
-		//ms6_166Hz = 4,
-		//ms7_142Hz = 4,
+		//ms5_200Hz = 5,
+		//ms6_166Hz = 6,
+		//ms7_142Hz = 7,
 		ms8_125Hz = 8,
 	}
 }
