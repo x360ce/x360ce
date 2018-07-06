9eec6d4216050f6074963530b22c2ea3e3977f9c
Evaldas Jocys <evaldas@jocys.com>
2018-07-06 12:37:01 +0100

    Version number fix

--- x360ce.App.Beta/Properties/AssemblyInfo.cs
+++ x360ce.App.Beta/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.8.11.0")]
-[assembly: AssemblyFileVersion("4.8.11.0")]
+[assembly: AssemblyVersion("4.8.0.11")]
+[assembly: AssemblyFileVersion("4.8.0.11")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
