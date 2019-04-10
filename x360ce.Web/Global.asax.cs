076471fe7017c5fac34c80ba4fc947dca49240f3
Evaldas Jocys <evaldas@jocys.com>
2019-04-10 17:07:18 +0100

    Web security updates.

--- x360ce.Web/Global.asax.cs
+++ x360ce.Web/Global.asax.cs
@@ -24,8 +24,9 @@ namespace x360ce.Web
 
 		protected void Application_Start(object sender, EventArgs e)
 		{
-
 			JocysCom.WebSites.Engine.Security.Check.CreateDefault();
+			// Fix Information Disclosure vulnerability by removing 'X-AspNetMvc-Version:' header from HTTP response. -->
+			//MvcHandler.DisableMvcResponseHeader = true;
 		}
 
 		protected void Session_Start(object sender, EventArgs e)
@@ -35,17 +36,16 @@ namespace x360ce.Web
 
 		protected void Application_BeginRequest(object sender, EventArgs e)
 		{
-			// Increase website security.
-			//The X - Frame - Options HTTP response header can be used To indicate whether Or Not 
+			// The X - Frame - Options HTTP response header can be used To indicate whether Or Not 
 			// a browser should be allowed To render a page In a <frame>, <iframe> or <object>.
 			// Sites can use this To avoid clickjacking attacks, by ensuring that their content
 			// Is Not embedded into other sites.
 			// DENY - The Page cannot be displayed In a frame, regardless of the site attempting to do so.
 			// SAMEORIGIN - The Page can only be displayed in a frame on the same origin as the page itself.
-			// ALLOW-From uri - The Page can only be displayed in a frame on the specified origin. 
-			HttpContext.Current.Response.AddHeader("x-frame-options", "SAMEORIGIN");
+			// ALLOW-From <URI> - The Page can only be displayed in a frame on the specified origin. 
+			HttpContext.Current.Response.AddHeader("X-Frame-Options", "SAMEORIGIN");
 			// Deny physical path.
-			if ((Request.Path.IndexOf((char)92) > -1 | System.IO.Path.GetFullPath(Request.PhysicalPath) != Request.PhysicalPath))
+			if (Request.Path.IndexOf('\\') > -1 || System.IO.Path.GetFullPath(Request.PhysicalPath) != Request.PhysicalPath)
 			{
 				throw new HttpException((int)System.Net.HttpStatusCode.NotFound, "Not Found");
 			}
@@ -75,6 +75,23 @@ namespace x360ce.Web
 				//	query["message"] = ex.Message;
 				//}
 				//WebHelper.Redirect("~/CustomError.aspx", query);
+			}
+		}
+
+		protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+		{
+			var app = sender as HttpApplication;
+			// Fix Information Disclosure vulnerability.
+			// Remove the HTTP Headers from response.
+			if (app != null && app.Request != null && !app.Request.IsLocal && app.Context != null && app.Context.Response != null)
+			{
+				var headers = app.Context.Response.Headers;
+				if (headers != null)
+				{
+					headers.Remove("Server");
+					headers.Remove("X-AspNet-Version");
+					headers.Remove("X-AspNetMvc-Version");
+				}
 			}
 		}
 
