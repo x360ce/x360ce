d0769ca7d62a70c3fb54c49002bd036e1867bd43
Evaldas Jocys <evaldas@jocys.com>
2019-05-26 15:45:20 +0100

    Adding test application for remote controller (work in progress).

---
+++ x360ce.RemoteController/OptionsWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JocysCom.x360ce.RemoteController
+{
+	/// <summary>
+	/// Interaction logic for OptionsWindow.xaml
+	/// </summary>
+	public partial class OptionsWindow : Window
+	{
+		public OptionsWindow()
+		{
+			InitializeComponent();
+			Title = new JocysCom.ClassLibrary.Configuration.AssemblyInfo().GetTitle(false, false, false, false, false, 0) + "- Options";
+
+		}
+	}
+}
