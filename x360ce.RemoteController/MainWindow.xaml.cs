f10b5eb3d28d5899bf4447ed6ddd37a4a2f6b809
Evaldas Jocys <evaldas@jocys.com>
2019-06-04 20:49:49 +0100

    Update remote controller test app.

--- x360ce.RemoteController/MainWindow.xaml.cs
+++ x360ce.RemoteController/MainWindow.xaml.cs
@@ -83,8 +83,7 @@ namespace JocysCom.x360ce.RemoteController
 				// If state changed.
 				if (currentState.Gamepad.Buttons != oldState.Gamepad.Buttons)
 				{
-					// Send state here (work in progress)
-					// ...
+					Send(currentState);
 					oldState = currentState;
 				}
 			}
@@ -124,6 +123,7 @@ namespace JocysCom.x360ce.RemoteController
 			}
 			var types = new Dictionary<Type, int>();
 			types.Add(typeof(State), 0);
+			types.Add(typeof(Gamepad), 1);
 			_Serializer = new ClassLibrary.Data.TlvSerializer(types);
 			if (Properties.Settings.Default.AutoConnect && Properties.Settings.Default.Connect)
 				StartServer();
@@ -145,14 +145,15 @@ namespace JocysCom.x360ce.RemoteController
 			currentState.Gamepad.Buttons &= ~list[s];
 		}
 
-		#region UDP Client
+		#region UDP Server
 
 		void AddLog(string format, params object[] args)
 		{
 			Trace.Write(string.Format(format, args));
 		}
 
-		UdpClient server;
+		/// <summary>Recevier</summary>
+		UdpClient udpServer;
 
 		public bool IsRunning { get; private set; }
 
@@ -165,14 +166,19 @@ namespace JocysCom.x360ce.RemoteController
 				return;
 			}
 			IsRunning = true;
+			Start();
 			// Set local endpoint (random port).
-			var localEndpoint = new IPEndPoint(IPAddress.Any, 0);
+			var localPort = FindFreePort();
+			var localEndpoint = new IPEndPoint(IPAddress.Any, localPort);
 			// Create client.
-			server = new UdpClient();
+			udpServer = new UdpClient();
 			// Workaround for: UDP SocketException - Only one usage of each socket address is normally permitted.
-			server.ExclusiveAddressUse = false;
-			server.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-			server.Client.Bind(localEndpoint);
+			udpServer.DontFragment = true;
+			udpServer.MulticastLoopback = false;
+			udpServer.ExclusiveAddressUse = false;
+			udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+			udpServer.Client.Bind(localEndpoint);
+			//InitClientServer();
 			AddLog("started.\r\n");
 			Receive();
 		}
@@ -186,7 +192,8 @@ namespace JocysCom.x360ce.RemoteController
 				return;
 			}
 			IsRunning = false;
-			server.Close();
+			Stop();
+			udpServer.Close();
 			AddLog("stopped.\r\n");
 		}
 
@@ -194,8 +201,8 @@ namespace JocysCom.x360ce.RemoteController
 		{
 			try
 			{
-				AddLog("Receiving on {0}...\r\n", server.Client.LocalEndPoint);
-				server.BeginReceive(new AsyncCallback(ReceiveCallBack), null);
+				AddLog("Receiving on {0}...\r\n", udpServer.Client.LocalEndPoint);
+				udpServer.BeginReceive(new AsyncCallback(ReceiveCallBack), null);
 			}
 			catch (Exception ex)
 			{
@@ -204,34 +211,21 @@ namespace JocysCom.x360ce.RemoteController
 			}
 		}
 
-		void Send(State state)
-		{
-			var ms = new MemoryStream();
-			var status = _Serializer.Serialize(ms, state);
-			if (status == ClassLibrary.Data.TlvSerializerError.None)
-			{
-				var bytes = ms.ToArray();
-				IPAddress adddress = IPAddress.Any;
-				IPAddress.TryParse(Properties.Settings.Default.ComputerHost, out adddress);
-				var remoteEndpoint = new IPEndPoint(adddress, Properties.Settings.Default.ComputerPort);
-				server.SendAsync(bytes, bytes.Length);
-			}
-		}
-
 		void ReceiveCallBack(IAsyncResult res)
 		{
 			// If disposed then simply return.
-			if (server.Client == null)
+			if (udpServer.Client == null)
 			{
 				IsRunning = false;
 				return;
 			}
 			try
 			{
-				var remoteEndpoint = new IPEndPoint(IPAddress.Any, 0);
-				var bytes = server.EndReceive(res, ref remoteEndpoint);
+				// Allow to receive from any source.
+				var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+				var bytes = udpServer.EndReceive(res, ref remoteEP);
 				var data = string.Join("", bytes.Select(x => x.ToString("X2")));
-				AddLog("Received Data from {0}: {1}\r\n", remoteEndpoint, data);
+				AddLog("Received Data from {0}: {1}\r\n", remoteEP, data);
 				if (!IsRunning)
 					Receive();
 			}
@@ -244,8 +238,62 @@ namespace JocysCom.x360ce.RemoteController
 			}
 		}
 
+		void Send(State state)
+		{
+			var ms = new MemoryStream();
+			var status = _Serializer.Serialize(ms, state);
+			var bytes = new byte[0];
+			if (status == ClassLibrary.Data.TlvSerializerError.None)
+			{
+				bytes = ms.ToArray();
+				IPAddress adddress;
+				IPAddress.TryParse(Properties.Settings.Default.ComputerHost, out adddress);
+				var remoteEndpoint = new IPEndPoint(adddress, Properties.Settings.Default.ComputerPort);
+				udpServer.Send(bytes, bytes.Length, remoteEndpoint);
+			}
+			var data = string.Join("", bytes.Select(x => x.ToString("X2")));
+			AddLog("{0:HH:mm:ss.fff} Send status: {1}\r\n", DateTime.Now, data);
+		}
 
 		#endregion
+
+		#region Helper Functions
+
+		/// <summary>
+		/// Find first free port. IANA Port categories:
+		///         0 –  1023 – System or Well Known ports.
+		///      1024 – 49151 – User or Registered ports.
+		///     49152 - 65535 – Dynamic (Private) or Ephemeral Ports.
+		/// </summary>
+		/// <returns>Free port number if found; otherwise 0.</returns>
+		private ushort FindFreePort(ushort startPort = 49152, ushort endPort = ushort.MaxValue)
+		{
+			var portArray = new List<int>();
+			var properties = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties();
+			// Get TCP connection ports.
+			var ports = properties.GetActiveTcpConnections()
+				.Where(x => x.LocalEndPoint.Port >= startPort)
+				.Select(x => x.LocalEndPoint.Port);
+			portArray.AddRange(ports);
+			// Get TCP listener ports.
+			ports = properties.GetActiveTcpListeners()
+				.Where(x => x.Port >= startPort)
+				.Select(x => x.Port);
+			portArray.AddRange(ports);
+			// Get UDP listener ports.
+			ports = properties.GetActiveUdpListeners()
+				.Where(x => x.Port >= startPort)
+				.Select(x => x.Port);
+			portArray.AddRange(ports);
+			// Get first port not in the list.
+			for (int i = startPort; i <= endPort; i++)
+				if (!portArray.Contains(i))
+					return (ushort)i;
+			return 0;
+		}
+
+		#endregion
+
 
 	}
 
