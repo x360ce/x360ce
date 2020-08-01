1930732a41e5046d483eaa3c4eb461a16e42d694
Evaldas Jocys <evaldas@jocys.com>
2020-08-01 14:04:07 +0100

    Digital Sing scripts updated.

--- x360ce.App.Beta/Documents/Step1_sign_module.XInput.bat
+++ x360ce.App.Beta/Documents/Step1_sign_module.XInput.bat
@@ -20,15 +20,17 @@ SET sgt=Tools\signtool.exe
 IF NOT EXIST "%sgt%" SET sgt=%ProgramFiles(x86)%\Windows Kits\10\App Certification Kit\signtool.exe
 IF NOT EXIST "%sgt%" SET sgt=%ProgramFiles(x86)%\Windows Kits\10\bin\x86\signtool.exe
 IF NOT EXIST "%sgt%" SET sgt=%ProgramFiles%\Windows Kits\10\bin\x86\signtool.exe
+echo %sgt%
+echo.
 :: Other options.
 set pfx=D:\_Backup\Configuration\SSL\Code Sign - Evaldas Jocys\2020\Evaldas_Jocys.pfx
 set d=X360CE - TocaEdit XBOX 360 Controller Emulator Library
-set du=http://www.x360ce.com
-set vsg=http://timestamp.verisign.com/scripts/timestamp.dll
+set du=https://www.x360ce.com
+set vsg=http://timestamp.comodoca.com
 if not exist "%sgt%" CALL:Error "%sgt%"
 if not exist "%~1"   CALL:Error "%~1"
 if not exist "%pfx%" CALL:Error "%pfx%"
-"%sgt%" sign /f "%pfx%" /d "%d%" /du "%du%" /t "%vsg%" /v "%~1"
+"%sgt%" sign /f "%pfx%" /d "%d%" /du "%du%" /fd sha256 /td sha256 /tr "%vsg%" /v "%~1"
 GOTO:EOF
 
 :Error
