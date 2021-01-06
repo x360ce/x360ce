73fd20f740a4846dd413e7870bb3cb08f99b93f9
Evaldas Jocys <evaldas@jocys.com>
2021-01-06 21:13:49 +0000

    UserDevices control update.

---
+++ x360ce.App.Beta/Converters/BoolToImageSourceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace x360ce.App.Converters
+{
+	public class BoolToImageSourceConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (bool)value
+				? Icons_Default.Current[Icons_Default.Icon_square_green]
+				: Icons_Default.Current[Icons_Default.Icon_square_grey];
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
--- x360ce.App.Beta/Resources/Icons/Icons_Default.xaml.cs
+++ x360ce.App.Beta/Resources/Icons/Icons_Default.xaml.cs
@@ -9,6 +9,9 @@ namespace x360ce.App
 			InitializeComponent();
 		}
 
+		public static Icons_Default Current => _Current = _Current ?? new Icons_Default();
+		private static Icons_Default _Current;
+
 		public const string Icon_add = nameof(Icon_add);
 		public const string Icon_arrow_circle = nameof(Icon_arrow_circle);
 		public const string Icon_arrow_circle2 = nameof(Icon_arrow_circle2);
