7abd8f9d8ad57674135a3859544cdf2159f24125
Evaldas Jocys <evaldas@jocys.com>
2022-10-25 22:53:58 +0100

    Update file links script.

--- x360ce.Engine/JocysCom/MakeLinks.ps1
+++ x360ce.Engine/JocysCom/MakeLinks.ps1
@@ -1,9 +1,9 @@
 <#
 .SYNOPSIS
-	Make/Fix soft file links to shared files.
+	Make/Fix soft or hard file links to shared files.
 .NOTES
     Author:     Evaldas Jocys <evaldas@jocys.com>
-    Modified:   2022-08-11
+    Modified:   2022-10-25
 .LINK
     http://www.jocys.com
 #>
@@ -36,7 +36,7 @@ if (-NOT ([WindowsPrincipal][WindowsIdentity]::GetCurrent()).IsInRole([WindowsBu
 $global:scriptName = $file.Basename;
 $global:scriptPath = $file.Directory.FullName;
 # Change current directory.
-[Console]::WriteLine("Script Path: {0}", $scriptPath);
+[Console]::WriteLine("Current Path: {0}", $scriptPath);
 [Environment]::CurrentDirectory = $scriptPath;
 Set-Location $scriptPath;
 # ----------------------------------------------------------------------------
@@ -55,45 +55,61 @@ function FindExistingPath
 #soft links (also called symlinks, or symbolic links)
 #hard links
 #junctions (a type of soft link only for directories)
-function MakeLink ($target, $link) {
-    # Deleting the target will cause soft links to stop working.
-    # What it points to is gone.
-    # Hard links will keep working until you delete the hard link itself.
-    # The hard link acts just like the original file, because for all intents and purposes, it is the original file.
+# Deleting the target will cause soft links to stop working.
+# What it points to is gone.
+# Hard links will keep working until you delete the hard link itself.
+# The hard link acts just like the original file, because for all intents and purposes, it is the original file.
+# ----------------------------------------------------------------------------
+function MakeSoftLink {
+    param([string]$link, [string]$target);
+    #----------------------------------------------------------
+    $linkType = (Get-Item $link).LinkType;
+    if ($linkType -eq "SymbolicLink"){
+        Write-Host "Skip Link: $link" -ForegroundColor DarkGray;
+        return;
+    }
+    Remove-Item $link;
+    $result = New-Item -Path $link -ItemType SymbolicLink -Value $target;
+    Write-Host "Make Link: $link";
 }
-
+# ----------------------------------------------------------------------------
+function MakeHardLink {
+    param([string]$link, [string]$target);
+    #----------------------------------------------------------
+    $linkType = (Get-Item $link).LinkType;
+    if ($linkType -eq "HardLink"){
+        Write-Host "Skip Link: $link" -ForegroundColor DarkGray;
+        return;
+    }
+    Remove-Item $link;
+    # Note: When hardlinked then there is no way to determine which file entry is the original.
+    $result = New-Item -Path $link -ItemType HardLink -Value $target;
+    Write-Host "Make Link: $link";
+}
 # ----------------------------------------------------------------------------
 $libraryPath = FindExistingPath @("c:\Projects\Jocys.com\Class Library", "d:\Projects\Jocys.com\Class Library");
 if ($null -eq $libraryPath){
     Write-Host "Library Path: Not Found!";
     return;
 }
-
 Write-Host "Library Path: $libraryPath";
+Write-Host;;
 
-[DirectoryInfo]$dir = New-Object DirectoryInfo ".";
 $items = Get-ChildItem "." -Recurse -Force | Where-Object {$_ -is [FileInfo]};
 foreach ($item in $items)
 {
-    #{ $_.Attributes -match "ReparsePoint" }
-    $fullName = $item.FullName;
-    $isLink = ((Get-Item $fullName).Attributes.ToString() -match "ReparsePoint");
-    $linkType = (Get-Item "$($item.FullName)").LinkType;
+    $linkFullName = $item.FullName;
     # Get relative name.
-    $relativeName = $fullName.Substring($dir.FullName.Length + 1);
-    # File must be in directory.
-    if (!$relativeName.Contains("\")){
+    $linkRelativeName = $linkFullName.Substring($scriptPath.Length + 1);
+    # If file is not in directory then continue.
+    if (!$linkRelativeName.Contains("\")){
         continue;
     }
-    Write-Host "  $relativeName";
-    # If normal file then remove.
-    if (!$isLink){
-        Remove-Item $fullName;
-        $target = "$libraryPath\$relativeName";
-        New-Item -Path $fullName -ItemType SymbolicLink -Value $target;
-        #Copy-Item $target -Destination $fullName;
-
-    }
+    # Original file for link to target.
+    $target = "$libraryPath\$linkRelativeName";
+    #MakeSoftLink $linkRelativeName $target;
+    MakeHardLink $linkRelativeName $target;
+    #Copy-Item $target -Destination $linkRelativeName;
 }
 # ----------------------------------------------------------------------------
 # Execute.
