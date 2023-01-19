4256d075f241475439f839b2501f4d94b13dcf42
Evaldas Jocys <evaldas@jocys.com>
2023-01-19 20:26:28 +0000

    Adding app UI Automated tests.

---
+++ x360ce.Net48Test/Common/x360ceAppUiTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+
+namespace x360ce.Tests
+{
+	[TestClass]
+	public class x360ceAppUiTest
+	{
+		string exePath = "..\\..\\..\\x360ce.App.Beta\\bin\\Debug\\x360ce.exe";
+		string winName = "Jocys.com X360 Controller Emulator";
+
+		[TestMethod]
+		public void Test_Start()
+		{
+			var di = new DirectoryInfo(".");
+			Console.WriteLine($"Current Path: {di.FullName}");
+			var p = Process.Start(exePath);
+			var window = AutomationHelper.FindWindow(p, new Regex("^" + winName, RegexOptions.IgnoreCase));
+			var info = window.Current;
+			var windowPattern = window.GetCurrentPattern(WindowPattern.Pattern) as WindowPattern;
+			Task.Delay(1000).Wait();
+			windowPattern.Close();
+			Task.Delay(1000).Wait();
+			p.Close();
+			Assert.IsTrue(true);
+
+		}
+
+	}
+}
