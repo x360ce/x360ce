3a3b1a0c7af473d0682f6920957c0962e3bc4e91
Evo Jocys <evo.jocys@gmail.com>
2026-09-23 00:00:33 +0100

    DOCS: v3 3.4.22.0 changelog and issue numbers for the issues this release closes
    v3 gets one section for everything since the last published v3 (3.2.10.82), and the version
    3.4.(6 + its lines). Lines for problems the published build never had are dropped; the
    force feedback test, .NET 4.6.2 and SetupDiGetDeviceRegistryPropertyW lines are added.
    v4 lines name #1634, #830 and #1038.

--- App.v3/Properties/AssemblyInfo.cs
+++ App.v3/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.15.0")]
-[assembly: AssemblyFileVersion("3.3.15.0")]
+[assembly: AssemblyVersion("3.4.22.0")]
+[assembly: AssemblyFileVersion("3.4.22.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
